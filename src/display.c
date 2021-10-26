303593c050a74d217ec75d36db2fd086e49594ce
David Wang <devtesting404@gmail.com>
2021-10-26 18:16:18 +0800

    fix display norme error

--- src/display.c
+++ src/display.c
@@ -46,7 +46,8 @@ static void	display_handler(t_vars *mlx, struct timeval *tv)
 	mlx->cam.p_coords.y = mlx->player_state.coords.y - P_TOP_OFFSET;
 	calc_w_cam(mlx->win_width, mlx->win_height, &mlx->cam);
 	mlx_put_image_to_window(mlx->mlx, mlx->win,
-		mlx->img_cache.static_assets.addr, mlx->cam.w_coords.x, mlx->cam.w_coords.y);
+		mlx->img_cache.static_assets.addr,
+		mlx->cam.w_coords.x, mlx->cam.w_coords.y);
 	display_info(mlx);
 	delay = (tv->tv_sec - mlx->player_state.last_move_time.tv_sec) * 1000
 		+ (tv->tv_usec - mlx->player_state.last_move_time.tv_usec) / 1000;
@@ -55,7 +56,8 @@ static void	display_handler(t_vars *mlx, struct timeval *tv)
 	else
 		p_img = loop_idle_player(&mlx->player_state, &mlx->img_cache.player, FPS);
 	calc_p_cam(&mlx->cam);
-	mlx_put_image_to_window(mlx->mlx, mlx->win, p_img, mlx->cam.p_coords.x, mlx->cam.p_coords.y);
+	mlx_put_image_to_window(mlx->mlx, mlx->win,
+		p_img, mlx->cam.p_coords.x, mlx->cam.p_coords.y);
 	mlx->player_state.state++;
 }
 
