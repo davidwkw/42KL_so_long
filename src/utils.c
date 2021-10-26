5de6496caddaffc96e4f302a2d7d2fd8d491e2b8
David Wang <devtesting404@gmail.com>
2021-10-26 18:11:19 +0800

    fix utils norme

--- src/utils.c
+++ src/utils.c
@@ -35,7 +35,8 @@ int	modulate_fps(int fps)
 	static struct timeval	prev;
 
 	gettimeofday(&tv, NULL);
-	if (tv.tv_sec == prev.tv_sec && (tv.tv_usec - prev.tv_usec) < (1000000 / fps))
+	if (tv.tv_sec == prev.tv_sec
+		&& (tv.tv_usec - prev.tv_usec) < (1000000 / fps))
 		return (0);
 	prev = tv;
 	return (1);
@@ -50,10 +51,12 @@ void	display_info(t_vars *mlx)
 
 	steps = ft_itoa(mlx->total_steps);
 	steps_str = ft_strjoin("Total steps : ", steps);
-	mlx_string_put(mlx->mlx, mlx->win, 20, mlx->win_height - 15, 0x00FFFFFF, steps_str);
+	mlx_string_put(mlx->mlx, mlx->win, 20,
+		mlx->win_height - 15, 0x00FFFFFF, steps_str);
 	num_coll = ft_itoa(mlx->map->collectible);
 	coll_str = ft_strjoin("Collectibles : ", num_coll);
-	mlx_string_put(mlx->mlx, mlx->win, 220, mlx->win_height - 15, 0x00FFFFFF, coll_str);
+	mlx_string_put(mlx->mlx, mlx->win, 220,
+		mlx->win_height - 15, 0x00FFFFFF, coll_str);
 	free(steps);
 	free(steps_str);
 	free(coll_str);
