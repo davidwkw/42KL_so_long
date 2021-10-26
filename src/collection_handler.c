bb5846a511d7e56aa31097660c385fbd1a7d6e27
David Wang <devtesting404@gmail.com>
2021-10-26 18:12:17 +0800

    fix collection_handler norme

--- src/collection_handler.c
+++ src/collection_handler.c
@@ -19,6 +19,7 @@ void	collection_handler(t_vars *mlx, int x, int y)
 
 	locate_p_collision(&mlx->ss_board, x, y, &coll_x, &coll_y, 'C');
 	map_tile(&mlx->ss_board, (coll_x / 100) * 100, (coll_y / 100) * 100, '0');
-	render_tile(&mlx->img_cache.static_assets, coll_x / 100, coll_y / 100, &mlx->img_cache.bg);
+	render_tile(&mlx->img_cache.static_assets,
+		coll_x / 100, coll_y / 100, &mlx->img_cache.bg);
 	mlx->map->collectible--;
 }
